b502b475874d26a0f8b86efc3930f8dc400b03c5
AnhMap <nguyenlephong1997@gmail.com>
2019-02-08 15:04:05 +0700

    update title and keyword

--- src/containers/layout/content/index.js
+++ src/containers/layout/content/index.js
@@ -3,7 +3,9 @@ import { Grid,  } from 'semantic-ui-react';
 import './content.scss';
 import ProfileSumary from 'containers/layout/content/info/ProfileSumary';
 import Education from 'containers/layout/content/info/Education';
-import Work from 'containers/layout/content/info/Work';
+import Skill from 'containers/layout/content/info/Skill';
+import Experience from 'containers/layout/content/info/Experience';
+import MyProject from 'containers/layout/content/info/MyProject';
 class ProfileContent extends React.Component {
     constructor(props) {
         super(props);
@@ -33,7 +35,23 @@ class ProfileContent extends React.Component {
                     <Grid.Column 
                         mobile={16} tablet={16} computer={16}
                         >
-                        <Work/>
+                        <Skill/>
+                        
+                    </Grid.Column>
+                </Grid.Row>
+                <Grid.Row columns={1} className="cv-content-block">
+                    <Grid.Column 
+                        mobile={16} tablet={16} computer={16}
+                        >
+                        <MyProject/>
+                        
+                    </Grid.Column>
+                </Grid.Row>
+                <Grid.Row columns={1} className="cv-content-block">
+                    <Grid.Column 
+                        mobile={16} tablet={16} computer={16}
+                        >
+                        <Experience/>
                         
                     </Grid.Column>
                 </Grid.Row>
