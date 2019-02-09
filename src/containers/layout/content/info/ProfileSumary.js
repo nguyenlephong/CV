0e0ac90c60866b1bb38d23fdb745e11f64ab9141
AnhMap <nguyenlephong1997@gmail.com>
2019-02-09 16:11:58 +0700

    update ui

--- src/containers/layout/content/info/ProfileSumary.js
+++ src/containers/layout/content/info/ProfileSumary.js
@@ -1,19 +1,43 @@
 import React from 'react';
+import './index.scss'; 
 
-import './index.scss';
 class ProfileSumary extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
 
     render() {
         return (
             <div className="cv-content-profile__sumary">
                 <h2 className="pf-sumary__title">Profile sumary</h2>
-                <span className="pf-sumary__description">
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
-                </span>
+                
+                <div className="pf-sumary__block_description">
+                    <span className="pf-sumary__description">
+                    Tên của tôi là Nguyễn Lê Phong, tôi hiện đang là sinh viên năm 4 của khoa công nghệ thông tin, trường đại học Nông Lâm Thành Phố Hồ Chí Minh.<span> </span> 
+                    </span>
+                    <span className="pf-sumary__description">
+                    Hiện tại tôi đã hoàn thành hết các môn học tín chỉ, kỳ này tôi đang trong giai đoạn làm luận văn tốt nghiệp.<span> </span>
+                    </span>
+                    <span className="pf-sumary__description">
+                    Tôi là một lập trình viên Fullstack, có kiến thức tốt về ngôn ngữ <strong>Java</strong>, <strong>Framework ReactJS</strong>.
+                    </span>
+                </div>
+
+                <div className="pf-sumary__block_description">
+                    <span className="pf-sumary__description">
+                    Tôi là người có trách nhiệm cao, thích giúp đỡ người khác, điều đó làm tôi trở nên dễ dàng thân thiện trong môi trường khác. <span> </span>
+                    </span>
+                    <span className="pf-sumary__description">
+                    Tôi được bạn bè đánh giá cao trong quá trình làm việc nhóm ở trường học qua các đồ án môn học. <span> </span>Tôi rất tự tin trong phần giao tiếp, thuyết trình một vấn đề. <span> </span>
+                    </span>
+                </div>
+
+                <div className="pf-sumary__block_description">
+                    <span className="pf-sumary__description">
+                    Công nghệ mới và ngôn ngữ mới là những chủ đề mà tôi đam mê, tìm hiểu và học hỏi. Ngay từ thời còn đi học tôi rất muốn áp dụng công nghệ mới để làm ra một vật thể có ích cho xã hội, và đó là đề tài làm luận văn tốt nghiệp của tôi.<span> </span>
+                    </span>
+                    <span className="pf-sumary__description">
+                    Tôi cũng có kênh youtube để chia sẽ kiến thức của mình đến với mọi người.<span> </span>
+                    </span>
+                </div>
+
             </div>
         );
     }
