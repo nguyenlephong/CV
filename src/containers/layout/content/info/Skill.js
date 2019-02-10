35de186273c35bb0d74ec28cc914b93af62001dd
AnhMap <nguyenlephong1997@gmail.com>
2019-02-10 11:58:24 +0700

    insert image

--- src/containers/layout/content/info/Skill.js
+++ src/containers/layout/content/info/Skill.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ProgrammingLanguageInfo from './image/ProgrammingLanguages.png';
 
 import './index.scss';
 class Work extends React.Component {
@@ -11,9 +12,12 @@ class Work extends React.Component {
         return (
             <div className="cv-content-profile__sumary">
                 <h2 className="pf-sumary__title">Skill</h2>
-                <span className="pf-sumary__description">
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
-                </span>
+                <div className="pf-skill__hardskill">
+                    <img src={ProgrammingLanguageInfo} alt="Programming languages" />
+                    <img src={ProgrammingLanguageInfo} alt="Programming languages" />
+                    <img src={ProgrammingLanguageInfo} alt="Programming languages" />
+                    <span className="pf-sumary__description"></span>
+                </div>
             </div>
         );
     }
